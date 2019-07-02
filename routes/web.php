180139ea6f9e09c67330857e4a9838a67ff0f010
swetha2019 <52072966+swetha2019@users.noreply.github.com>
2019-07-02 17:53:34 +0530

    changes on 02-07-2019

--- routes/web.php
+++ routes/web.php
@@ -46,10 +46,17 @@ Route::get('/changePassword','HomeController@showChangePasswordForm');
 Route::get('admin',['uses'=>'admin\admin_controller@index']);
 Route::post('adminlogin','admin\admin_controller@adminlogin');
 Route::get('adminhome','admin\admin_controller@vendor_detail');
-Route::get('approved/{id}','admin\admin_controller@aPProved');
+Route::get('approved/{id}','admin\admin_controller@aPProved');//for activate the the vendores
 Route::get('vendor_edit/{id}','admin\admin_controller@vendor_edit');
 Route::get('notification/{id}','admin\admin_controller@notification');
 Route::get('admin_edit/{id}','admin\admin_controller@admin_edit')->name('admin_edit');
 Route::post('admin_update/{id}','admin\admin_controller@admin_update');//for update admin detail
 Route::post('admin_change_password/{id}','admin\admin_controller@admin_change_password');
+Route::get('Vendor_Delete/{id}','admin\admin_controller@Vendor_Delete');
+
+Route::post('/search','admin\admin_controller@search');
+
 Route::get('admin_logout','admin\admin_controller@admin_logout');
+
+
+
