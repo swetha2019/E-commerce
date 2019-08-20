b8e91055a7b9693c66a0556b6b4ea1e4f4068027
swetha2019 <52072966+swetha2019@users.noreply.github.com>
2019-08-20 13:06:19 +0530

    updated at 20-08-2019

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/', 'user_controll\UserController@product_view')->name('product_view');
+Route::get('/vendor', function () {
     return view('auth.login');
 });
 Route::get('profile','HomeController@edit');
@@ -42,8 +42,8 @@ Route::get('/changePassword','HomeController@showChangePasswordForm');
 
 
 
-//Admin login ---------------------------------------
-//Admin login ---------------------------------------
+
+//--------------------------Admin login ---------------------------------------
 Route::get('admin',['uses'=>'admin\admin_controller@index']);
 Route::post('adminlogin','admin\admin_controller@adminlogin');
 Route::get('adminhome','admin\admin_controller@vendor_detail');
@@ -75,7 +75,10 @@ Route::post('add_categories','admin\admin_controller@add_categories');//add cate
 Route::get('add_subcategory','admin\admin_controller@add_subcategory');
 Route::post('add_subcategories','admin\admin_controller@add_subcategories');//for adding sub categories
 Route::get('Catgory_delete/{id}','admin\admin_controller@Catgory_delete');
-Route::get('Catgory_edit/{id}','admin\admin_controller@Catgory_edit');
+Route::get('catgory_edit/{id}','admin\admin_controller@catgory_edit');
+Route::get('sub_category_delete/{id}','admin\admin_controller@sub_category_delete');
+Route::get('sub_category_edit/{id}','admin\admin_controller@sub_category_edit');
+Route::post('upadet_subcategories/{id}','admin\admin_controller@upadet_subcategories');
 //Route::post('/search','admin\admin_controller@search');
 Route::get('add_Childcategory','admin\admin_controller@add_Childcategory');
 Route::get('add_Childcategory2','admin\admin_controller@add_Childcategory2');
@@ -86,6 +89,7 @@ Route::post('childcategory','admin\admin_controller@childcategory');
 Route::get('new_vendor','admin\admin_controller@new_vendor');
 Route::get('rect_vendor','admin\admin_controller@rect_vendor');
 Route::get('child_select','admin\admin_controller@child_select');
+Route::post('update_categories/{id}','admin\admin_controller@update_categories');
 
 //vendor routes
 Route::get('/delete','HomeController@delete');
@@ -96,4 +100,56 @@ Route::post('/subscription','HomeController@subscription');
 Route::post('/get_subscription','PaymentController@get_subscription');
 Route::get('/pg_redirect','PaymentController@pg_redirect');
 
+// Route::get('test',function()
+// {
+// 	return view('test');
+// });
+// Route::post('multi','controller@multi');
+// Route::get('disp','controller@diso');
 
+
+
+//------------------prodct---------------------------------------------------
+Route::get('product','admin\admin_controller@product');
+Route::get('product_delete/{id}','admin\admin_controller@product_delete');
+Route::get('product_detail/{id}','admin\admin_controller@product_detail');
+
+Route::get('subscription','admin\admin_controller@subscription');
+Route::get('subscription_delete/{id}','admin\admin_controller@subscription_delete');
+// Route::get('product_detail/{id}','admin\admin_controller@product_detail');
+Route::post('pro_edit/{id}','PaymentController@pro_edit');
+
+//--------------home controller--------------------------------------
+Route::post('/edit','HomeController@edit');
+//Route::post('/subscribe','HomeController@subscribe');
+Route::get('/delete','HomeController@delete');
+Route::get('/logout','HomeController@logout')->name('logout');
+Route::get('/edit_profile', 'HomeController@countries')->name('countries');
+Route::get('/district','HomeController@district')->name('district');
+Route::post('/subscription','HomeController@subscription');
+Route::get('/change_password','HomeController@change_password')->name('change_password');
+Route::post('/update_password','HomeController@update_password');
+
+Route::post('/get_subscription','PaymentController@get_subscription');
+Route::get('/pg_redirect','PaymentController@pg_redirect');
+
+Route::get('/add_redirect','ProductController@add_redirect');
+Route::post('/add_product','ProductController@add_product');
+Route::post('/add_category','ProductController@add_category');
+Route::post('/add_sub_category','ProductController@add_sub_category');
+Route::post('/add_new_sub_category','ProductController@add_new_sub_category');
+Route::get('/product_delete/{id}','ProductController@product_delete');
+Route::post('/category','ProductController@category');
+Route::get('/sub_category','ProductController@sub_category')->name('sub_category');
+Route::get('/child_category1','ProductController@child_category1')->name('child_category1');
+Route::get('/child_category2','ProductController@child_category2')->name('child_category2');
+Route::post('/update_image','ProductController@update_image');
+
+Route::get('/view_product','ProductController@view_product');
+Route::get('/delete_product/{id}','ProductController@delete_product');
+Route::get('/edit_product/{id}','ProductController@edit_product')->name('edit_product');
+Route::get('/product_image_view/{id}','ProductController@product_image_view')->name('product_image_view');
+Route::get('/view_product_details/{id}','ProductController@view_product_details')->name('view_product_details');
+Route::post('/update_product','ProductController@update_product');
+Route::get('/product_count','ProductController@product_count');
+Route::get('/paytm_redirect','ProductController@paytm_redirect');
